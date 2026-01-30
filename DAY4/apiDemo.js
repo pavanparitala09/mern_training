85fdf40cdff406ce8aa7b1c6d52a5bb18a5e3c54
pavanparitala09 <pavankumarparitala2580@gmail.com>
2026-01-30 22:32:08 +0530

    Removed old files after reorganization

--- DAY4/apiDemo.js
+++
@@ -1,5 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .then(json => console.log(json))
-      .catch(err => console.log("error i :",err))
